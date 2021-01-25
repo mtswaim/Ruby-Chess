8d55675bca92e6705f9697136caa2fb566471d13
Matt Swaim <matthew.swaim@matthewswaim-2020.local>
2021-01-25 16:00:33 -0600

    Piece Creation
    Created the following piece classes with initialize
        -Knight
        -Rook
        -Pawn
        -Bishop
        -Queen
        -King
    Created the piece Class that will be the parent for all pieces, meant to outline what pieces can and cant do
    Created the is_out_of_bounds method to check if a piece is out of bounds when a move it attempted

--- chess.rb
+++ chess.rb
@@ -1,42 +1,66 @@
 class Board
-    attr_accessor :grid, :knight
+    attr_accessor :grid
     def initialize
         @grid = Array.new(8) {Array.new(8, " ") }
-        @white_king = "♔"
-        @white_queen = "♕"
-        @white_rook = "♖"
-        @white_bishop = "♗"
-        @white_knight = "♘"
-        @white_pawn = "♙"
-        @black_king = "♚"
-        @black_queen = "♛"
-        @black_rook = "♜"
-        @black_bishop = "♝"
-        @black_knight = "♞"
-        @black_pawn = "♟" 
     end
     def to_s
         @grid.each do | line | 
-        print "\n"
-        @horizontal = "+----+----+----+----+----+----+----+----+"
-        puts @horizontal
-        line.each do | slot |
-        print "| #{slot}  "
-        end
-        print "|"
+            print "\n"
+            @horizontal = "+----+----+----+----+----+----+----+----+"
+            puts @horizontal
+            line.each do | slot |
+                print "| #{slot}  "
+            end
+            print "|"
         end
         print "\n"
         print @horizontal
     end
 end
-
 class Piece
+    def is_out_of_bounds(x, y)
+        if x > 9 || y > 9 || x < 0 || y < 0
+            puts "You freaking idiot, make a move inside the board"
+        end
+    end
+end
+class Pawn 
+    def initialize(color,x,y)
+        @icon = color == "white" ? "♙" : "♟"
+    end
 end
 
-class Rook
-    
+class Knight 
+    def initialize(color,x,y)
+        @icon = color == "white" ? "♘" : "♞"
+    end
+end
+
+class Rook 
+    def initialize(color,x,y)
+        @icon = color == "white" ? "♖" : "♜"
+    end
+end
+
+class Bishop 
+    def initialize(color,x,y)
+        @icon = color == "white" ? "♗" : "♝"
+    end
+end
+
+class King 
+    def initialize(color,x,y)
+        @icon = color == "white" ? "♔" : "♚"
+    end
+end
+
+class Queen
+    def initialize(color,x,y)
+        @icon = color == "white" ? "♕" : "♛"
+    end
 end
 
 board = Board.new
-board.knight
+rook = Rook.new("white",0,0)
+board.rook
 puts(board)
